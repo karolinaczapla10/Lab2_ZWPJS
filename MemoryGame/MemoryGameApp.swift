155c8389085fbdeee1842e6484dfd1efaa6d44cb
Karolina <karolinaczapla101@gmail.com>
2024-10-22 10:21:06 +0200

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Karolina on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
